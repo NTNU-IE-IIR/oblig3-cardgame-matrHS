8248a95556ec5b3efa500cb98d368ab683fb8e61
matrHS <matthew.r.hunt@gmail.com>
2024-02-19 14:29:47 +0100

    Functionality added to DeckOfCards. Helper method to print cards

---
+++ src/main/java/edu/ntnu/matthew/DeckOfCards.java
@@ -0,0 +1,63 @@
+package edu.ntnu.matthew;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import no.ntnu.idatx2003.oblig3.cardgame.PlayingCard;
+
+/**
+ * Represents a deck of cards.
+ */
+public class DeckOfCards {
+  private final char[] suits = { 'S', 'H', 'D', 'C' };
+  private ArrayList<PlayingCard> deck;
+
+  /**
+   * Constructor for the deck.
+   * Fills deck with 52 cards, 13 of each suit.
+   */
+  public DeckOfCards() {
+    this.deck = new ArrayList<PlayingCard>();
+    for (char suit : suits) {
+      for (int face = 1; face <= 13; face++) {
+        this.addCard(new PlayingCard(suit, face));
+      }
+    }
+  }
+
+  /**
+   * Adds a card to the deck.
+   * 
+   * @param card The card to add to the deck.
+   * @throws IllegalArgumentException if card is null.
+   */
+  public void addCard(PlayingCard card) {
+    if (card == null) {
+      throw new IllegalArgumentException("Card cannot be null");
+    }
+    
+    this.deck.add(card);
+  }
+
+  /**
+   * Returns number of cards representing hand.
+   * 
+   * @param n Number of cards to deal.
+   * @return Collection of cards representing hand.
+   */
+  public Collection<PlayingCard> dealHand(int n) {
+    Collection<PlayingCard> hand = new ArrayList<PlayingCard>();
+    
+    
+    
+    return hand;
+  }
+  
+  /**
+   * Returns entire deck as collection.
+   * 
+   * @return Collection of cards representing deck.
+   */
+  public Collection<PlayingCard> getDeck() {
+    return this.deck;
+  }
+}
--- src/main/java/edu/ntnu/matthew/MainApp.java
+++ src/main/java/edu/ntnu/matthew/MainApp.java
@@ -1,7 +1,17 @@
 package edu.ntnu.matthew;
 
+import java.util.Collection;
+import no.ntnu.idatx2003.oblig3.cardgame.PlayingCard;
+
 public class MainApp {
   public static void main(String[] args) {
-    
+    DeckOfCards deck = new DeckOfCards();
+    printCards(deck.getDeck());
+  }
+  
+  public static void printCards(Collection<PlayingCard> cards) {
+    for (PlayingCard card : cards) {
+      System.out.println(card.getFace() + " of " + card.getSuit());
+    }
   }
 }
